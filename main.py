b5b4ed7be1dd53f11fb7095f6d8a3633b1cd677f
Julio Dominguez Perez <chiapasjeremai@gmail.com>
2022-04-24 19:25:22 -0500

    Calculando el indice masa corporal

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+print("ingrese sus datos por favor")
+print("Ingrese su peso en kg")
+peso=float(input())
+print("Su estatura en metros")
+estatura=float(input())
+es = peso/(estatura*estatura);
+
+
+print("El indice de masa corporal es de: ",round(es,2))
